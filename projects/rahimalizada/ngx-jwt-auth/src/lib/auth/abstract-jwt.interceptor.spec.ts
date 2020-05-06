76b463881dec0dddac95c3c63cb7de7c44c1d79c
Rahim Alizada <rahim.alizada@gmail.com>
2020-05-07 00:39:52 +0400

    Created AbstractJwtInterceptor

---
+++ projects/rahimalizada/ngx-jwt-auth/src/lib/auth/abstract-jwt.interceptor.spec.ts
@@ -0,0 +1,214 @@
+import { Location } from '@angular/common';
+import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Component, Injectable } from '@angular/core';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { Router, Routes } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TestAuthService } from './abstract-auth.service.spec';
+import { AbstractJwtInterceptor } from './abstract-jwt.interceptor';
+@Component({ template: '<router-outlet></router-outlet>' })
+class AppComponent {}
+
+@Component({ template: 'Login' })
+class LoginComponent {}
+
+const routes: Routes = [{ path: 'test/auth/login', component: LoginComponent }];
+
+@Injectable({ providedIn: 'root' })
+class DataService {
+  constructor(private http: HttpClient) {}
+
+  run() {
+    return this.http.get<string>('http://localhost/path');
+  }
+}
+
+@Injectable()
+class TestJWTInterceptor<T extends { token: string; refreshToken: string; roles: string[] }> extends AbstractJwtInterceptor<T> {
+  constructor(authService: TestAuthService<T>, r: Router) {
+    super(authService, r, '/test/auth/login');
+  }
+}
+
+let dataService: DataService;
+let httpTestingController: HttpTestingController;
+let location: Location;
+
+function sharedSetup() {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoginComponent, AppComponent],
+      imports: [RouterTestingModule.withRoutes(routes), HttpClientTestingModule],
+      providers: [{ provide: HTTP_INTERCEPTORS, useClass: TestJWTInterceptor, multi: true }],
+    });
+    dataService = TestBed.inject(DataService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    location = TestBed.inject(Location);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+    localStorage.removeItem(TestAuthService.storageItemId);
+  });
+}
+
+describe('AbstractJwtInterceptor without existing token', () => {
+  sharedSetup();
+
+  it('should not add an Authorization header', () => {
+    dataService.run().subscribe((res) => {});
+    const req = httpTestingController.expectOne('http://localhost/path');
+    expect(req.request.method).toEqual('GET');
+    expect(req.request.headers.has('Authorization')).toEqual(false);
+    req.flush('ok');
+  });
+});
+
+describe('AbstractJwtInterceptor with existing expired token', () => {
+  beforeEach(() => {
+    localStorage.setItem(TestAuthService.storageItemId, JSON.stringify(TestAuthService.expiredAuthData));
+  });
+  sharedSetup();
+
+  it('should renew and add an Authorization header', () => {
+    dataService.run().subscribe(
+      () => {},
+      (err) => expect(err.status).toBe(401),
+    );
+
+    const req = httpTestingController.expectOne('/api/auth/renew-token');
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.headers.has('Authorization')).toEqual(false);
+    req.flush(TestAuthService.validAuthData);
+
+    const req2 = httpTestingController.expectOne('http://localhost/path');
+    expect(req2.request.method).toEqual('GET');
+    expect(req2.request.headers.has('Authorization')).toEqual(true);
+    req2.flush('ok');
+  });
+
+  it('should fail renew and stay on home page', fakeAsync(() => {
+    dataService.run().subscribe(
+      () => {},
+      (err) => expect(err.status).toBe(404),
+    );
+
+    const req = httpTestingController.expectOne('/api/auth/renew-token');
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.headers.has('Authorization')).toEqual(false);
+    req.flush('Error message', { status: 404, statusText: 'Not Found' });
+
+    tick();
+    expect(location.path()).toBe('');
+  }));
+
+  it('should fail renew and go to login page', fakeAsync(() => {
+    dataService.run().subscribe(
+      () => {},
+      (err) => expect(err.status).toBe(401),
+    );
+
+    const req = httpTestingController.expectOne('/api/auth/renew-token');
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.headers.has('Authorization')).toEqual(false);
+    req.flush('Error message', { status: 401, statusText: 'Unauthorized' });
+
+    tick();
+    expect(location.path()).toBe('/test/auth/login');
+  }));
+
+  it('should renew and fail the result with 404', fakeAsync(() => {
+    dataService.run().subscribe(
+      () => {},
+      (err) => expect(err.status).toBe(404),
+    );
+
+    const req = httpTestingController.expectOne('/api/auth/renew-token');
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.headers.has('Authorization')).toEqual(false);
+    req.flush(TestAuthService.validAuthData);
+
+    const req2 = httpTestingController.expectOne('http://localhost/path');
+    expect(req2.request.method).toEqual('GET');
+    expect(req2.request.headers.has('Authorization')).toEqual(true);
+    req2.flush('Error message', { status: 404, statusText: 'Not Found' });
+
+    tick();
+    expect(location.path()).toBe('');
+  }));
+
+  it('should renew and fail the result with 401', fakeAsync(() => {
+    dataService.run().subscribe(
+      () => {},
+      (err) => expect(err.status).toBe(401),
+    );
+
+    const req = httpTestingController.expectOne('/api/auth/renew-token');
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.headers.has('Authorization')).toEqual(false);
+    req.flush(TestAuthService.validAuthData);
+
+    const req2 = httpTestingController.expectOne('http://localhost/path');
+    expect(req2.request.method).toEqual('GET');
+    expect(req2.request.headers.has('Authorization')).toEqual(true);
+    req2.flush('Error message', { status: 401, statusText: 'Unauthorized' });
+
+    tick();
+    expect(location.path()).toBe('/test/auth/login');
+  }));
+});
+
+describe('AbstractJwtInterceptor with existing valid token', () => {
+  beforeEach(() => {
+    localStorage.setItem(TestAuthService.storageItemId, JSON.stringify(TestAuthService.validAuthData));
+  });
+  sharedSetup();
+
+  it('should add an Authorization header without renewal', () => {
+    dataService.run().subscribe(() => {});
+
+    httpTestingController.expectNone('/api/auth/renew-token');
+
+    const req = httpTestingController.expectOne('http://localhost/path');
+    expect(req.request.method).toEqual('GET');
+    expect(req.request.headers.has('Authorization')).toEqual(true);
+    req.flush('ok');
+  });
+
+  it('should not add an Authorization header on 404', () => {
+    dataService.run().subscribe(
+      () => {},
+      (err) => expect(err.status).toBe(404),
+    );
+
+    const req = httpTestingController.expectOne('http://localhost/path');
+    expect(req.request.method).toEqual('GET');
+    expect(req.request.headers.has('Authorization')).toEqual(true);
+    req.flush('Error message', { status: 404, statusText: 'Not Found' });
+
+    httpTestingController.expectNone('/api/auth/renew-token');
+  });
+
+  it('should not add an Authorization header on 401', () => {
+    dataService.run().subscribe(
+      () => {},
+      (err) => expect(err.status).toBe(401),
+    );
+
+    const req = httpTestingController.expectOne('http://localhost/path');
+    expect(req.request.method).toEqual('GET');
+    expect(req.request.headers.has('Authorization')).toEqual(true);
+    req.flush('Error message', { status: 401, statusText: 'Unauthorized' });
+
+    const req2 = httpTestingController.expectOne('/api/auth/renew-token');
+    expect(req2.request.method).toEqual('POST');
+    expect(req2.request.headers.has('Authorization')).toEqual(false);
+    req2.flush(TestAuthService.validAuthData);
+
+    const req3 = httpTestingController.expectOne('http://localhost/path');
+    expect(req3.request.method).toEqual('GET');
+    expect(req3.request.headers.has('Authorization')).toEqual(true);
+    req3.flush('ok');
+  });
+});
---
+++ projects/rahimalizada/ngx-jwt-auth/src/lib/auth/abstract-jwt.interceptor.ts
@@ -0,0 +1,71 @@
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable, throwError } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
+import { AbstractAuthService } from './abstract-auth.service';
+
+export abstract class AbstractJwtInterceptor<T extends { token: string; refreshToken: string; roles: string[] }>
+  implements HttpInterceptor {
+  constructor(private authService: AbstractAuthService<T>, private router: Router, private tokenRenewalfailRedirect: string) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.authService.authDataSubject.value ? this.authService.authDataSubject.value.token : null;
+    const tokenExpired = token ? this.authService.isTokenExpired(token) : true;
+    const isAuthPath = req.url.search(this.authService.apiPath) === 0; // starts with /auth/...
+
+    if (isAuthPath) {
+      return next.handle(req);
+    } else if (token && tokenExpired) {
+      //
+      console.log('Token has expired, renewing the token and trying again');
+      return this.tryWithRenewedToken(req, next);
+    } else if (token) {
+      //
+      return next.handle(this.addToken(req)).pipe(
+        catchError((error: any) => {
+          if (error instanceof HttpErrorResponse && error.status === 401) {
+            console.log('Request failed with 401 status code, renewing the token and trying again');
+            return this.tryWithRenewedToken(req, next);
+          } else {
+            return throwError(error);
+          }
+        }),
+      );
+    }
+
+    return next.handle(req);
+  }
+
+  tryWithRenewedToken(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return this.authService.renewToken().pipe(
+      switchMap((authData) => {
+        console.log('Retrying request with renewed token');
+        return next.handle(this.addToken(req)).pipe(
+          catchError((error) => {
+            if (error instanceof HttpErrorResponse && error.status === 401) {
+              console.log('Request retry with renewed token failed with 401 status code, logging out');
+            }
+            return throwError(error);
+          }),
+        );
+      }),
+      catchError((error) => {
+        if (error instanceof HttpErrorResponse && error.status === 401) {
+          console.log('Token renewal failed with 401 status code, logging out');
+          // If there is an exception calling 'renewToken', No need to logout though, renewToken pipe did that already
+          // this.authService.logout();
+          this.router.navigate([this.tokenRenewalfailRedirect]);
+        }
+        return throwError(error);
+      }),
+    );
+  }
+
+  private addToken(req: HttpRequest<any>): HttpRequest<any> {
+    return req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${this.authService.authDataSubject.value.token}`,
+      },
+    });
+  }
+}
