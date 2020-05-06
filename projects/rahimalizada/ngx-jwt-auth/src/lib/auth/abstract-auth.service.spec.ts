7f81667aa4c0394cd7c395369b6f9a677eaade75
Rahim Alizada <rahim.alizada@gmail.com>
2020-05-07 00:39:34 +0400

    Created AbstractAuthService

---
+++ projects/rahimalizada/ngx-jwt-auth/src/lib/auth/abstract-auth.service.spec.ts
@@ -0,0 +1,308 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Injectable } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { AbstractAuthService } from '../../public-api';
+
+export class Constants {}
+
+@Injectable({ providedIn: 'root' })
+export class TestAuthService<T extends { token: string; refreshToken: string; roles: string[] }> extends AbstractAuthService<T> {
+  public static readonly storageItemId = 'test-storage';
+  public static readonly apiPath = '/api/auth';
+
+  public static readonly validToken =
+    // tslint:disable-next-line: max-line-length
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE5MTYyMzkwMjJ9.dMD3MLuHTiO-Qy9PvOoMchNM4CzFIgI7jKVrRtlqlM0';
+  public static readonly expiredToken =
+    // tslint:disable-next-line: max-line-length
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE1MTYyMzkwMjJ9.4Adcj3UFYzPUVaVF43FmMab6RlaQD8A9V8wFzzht-KQ';
+
+  public static readonly validAuthData = { token: TestAuthService.validToken, refreshToken: TestAuthService.validToken, roles: ['*'] };
+  public static readonly expiredAuthData = {
+    token: TestAuthService.expiredToken,
+    refreshToken: TestAuthService.expiredToken,
+    roles: ['read', 'write'],
+  };
+  public static readonly invalidAuthData = { token: 'invalid', refreshToken: null, roles: ['-'] };
+
+  constructor(httpClient: HttpClient) {
+    super(TestAuthService.storageItemId, httpClient, TestAuthService.apiPath);
+  }
+}
+
+let service: TestAuthService<any>;
+let httpTestingController: HttpTestingController;
+
+function sharedSetup() {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.inject(TestAuthService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+    localStorage.removeItem(TestAuthService.storageItemId);
+  });
+}
+
+describe('AbstractAuthService', () => {
+  sharedSetup();
+
+  it('should be created when state is missing from local storage', () => {
+    expect(service.isLoggedIn()).toBe(false);
+  });
+
+  it('should submit valid reset password request', () => {
+    service.resetPasswordRequest({}).subscribe((res) => {});
+
+    const req = httpTestingController.expectOne('/api/auth/reset-password/request');
+    expect(req.request.method).toEqual('POST');
+    req.flush(TestAuthService.validAuthData);
+  });
+
+  it('should submit valid reset password confirmation', () => {
+    service.resetPasswordConfirmation({}).subscribe((res) => {});
+
+    const req = httpTestingController.expectOne('/api/auth/reset-password/confirmation');
+    expect(req.request.method).toEqual('POST');
+    req.flush(TestAuthService.validAuthData);
+  });
+});
+
+describe('AbstractAuthService with invalid state in local storage', () => {
+  beforeEach(() => {
+    localStorage.setItem(TestAuthService.storageItemId, JSON.stringify(TestAuthService.invalidAuthData));
+  });
+  sharedSetup();
+
+  it('should load invalid state', () => {
+    expect(service.isLoggedIn()).toBe(false);
+  });
+
+  it('should test roles properly', () => {
+    expect(service.hasPermissions('*')).toBe(false);
+    expect(service.hasPermissions('read')).toBe(false);
+    expect(service.hasPermissions('write')).toBe(false);
+    expect(service.hasPermissions('read,write')).toBe(false);
+    expect(service.hasPermissions('read, write')).toBe(false);
+    expect(service.hasPermissions('read,write,update')).toBe(false);
+    expect(service.hasPermissions('read, write, update')).toBe(false);
+    expect(service.hasPermissions('-')).toBe(true);
+  });
+});
+
+describe('AbstractAuthService with expired state in local storage', () => {
+  beforeEach(() => {
+    localStorage.setItem(TestAuthService.storageItemId, JSON.stringify(TestAuthService.expiredAuthData));
+  });
+  sharedSetup();
+
+  it('should load expired state', () => {
+    expect(service.isLoggedIn()).toBe(false);
+  });
+
+  it('should test roles properly', () => {
+    expect(service.hasPermissions('*')).toBe(false);
+    expect(service.hasPermissions('read')).toBe(true);
+    expect(service.hasPermissions('write')).toBe(true);
+    expect(service.hasPermissions('read,write')).toBe(true);
+    expect(service.hasPermissions('read, write')).toBe(false);
+    expect(service.hasPermissions('read,write,update')).toBe(false);
+    expect(service.hasPermissions('read, write, update')).toBe(false);
+    expect(service.hasPermissions('-')).toBe(false);
+  });
+});
+
+describe('AbstractAuthService with valid state in local storage', () => {
+  beforeEach(() => {
+    localStorage.setItem(TestAuthService.storageItemId, JSON.stringify(TestAuthService.validAuthData));
+  });
+  sharedSetup();
+
+  it('should load valid state', () => {
+    expect(service.isLoggedIn()).toBe(true);
+  });
+
+  it('should delete the state on logout', () => {
+    service.logout();
+    expect(service.isLoggedIn()).toBe(false);
+    expect(localStorage.getItem(TestAuthService.storageItemId)).toBeNull();
+  });
+
+  it('should test roles properly', () => {
+    expect(service.hasPermissions('*')).toBe(true);
+    expect(service.hasPermissions('read')).toBe(true);
+    expect(service.hasPermissions('write')).toBe(true);
+    expect(service.hasPermissions('read,write')).toBe(true);
+    expect(service.hasPermissions('read, write')).toBe(true);
+    expect(service.hasPermissions('read,write,update')).toBe(true);
+    expect(service.hasPermissions('read, write, update')).toBe(true);
+    expect(service.hasPermissions('-')).toBe(true);
+  });
+});
+
+describe('AbstractAuthService login', () => {
+  sharedSetup();
+
+  it('should save expired state to local storage', () => {
+    service.login({}).subscribe((res) => {
+      expect(service.isLoggedIn()).toBe(false);
+      expect(localStorage.getItem(TestAuthService.storageItemId)).not.toBeNull();
+    });
+
+    const req = httpTestingController.expectOne('/api/auth/login');
+    expect(req.request.method).toEqual('POST');
+    req.flush(TestAuthService.expiredAuthData);
+  });
+
+  it('should save invalid state to local storage', () => {
+    service.login({}).subscribe((res) => {
+      expect(service.isLoggedIn()).toBe(false);
+      expect(localStorage.getItem(TestAuthService.storageItemId)).not.toBeNull();
+    });
+
+    const req = httpTestingController.expectOne('/api/auth/login');
+    expect(req.request.method).toEqual('POST');
+    req.flush(TestAuthService.invalidAuthData);
+  });
+
+  it('should save valid state to local storage', () => {
+    service.login({}).subscribe((res) => {
+      expect(service.isLoggedIn()).toBe(true);
+      expect(localStorage.getItem(TestAuthService.storageItemId)).not.toBeNull();
+    });
+
+    const req = httpTestingController.expectOne('/api/auth/login');
+    expect(req.request.method).toEqual('POST');
+    req.flush(TestAuthService.validAuthData);
+  });
+
+  it('should logot on invalid result', () => {
+    service.login({}).subscribe(
+      (res) => {},
+      (err) => {
+        expect(service.isLoggedIn()).toBe(false);
+        expect(localStorage.getItem(TestAuthService.storageItemId)).toBeNull();
+        expect(err.status).toBe(404);
+      },
+    );
+
+    const req = httpTestingController.expectOne('/api/auth/login');
+    expect(req.request.method).toEqual('POST');
+    req.flush('Error message', { status: 404, statusText: 'Not Found' });
+  });
+});
+
+describe('AbstractAuthService registration', () => {
+  sharedSetup();
+
+  it('should save expired state to local storage', () => {
+    service.register({}).subscribe((res) => {
+      expect(service.isLoggedIn()).toBe(false);
+      expect(localStorage.getItem(TestAuthService.storageItemId)).not.toBeNull();
+    });
+
+    const req = httpTestingController.expectOne('/api/auth/register');
+    expect(req.request.method).toEqual('POST');
+    req.flush(TestAuthService.expiredAuthData);
+  });
+
+  it('should save invalid state to local storage', () => {
+    service.register({}).subscribe((res) => {
+      expect(service.isLoggedIn()).toBe(false);
+      expect(localStorage.getItem(TestAuthService.storageItemId)).not.toBeNull();
+    });
+
+    const req = httpTestingController.expectOne('/api/auth/register');
+    expect(req.request.method).toEqual('POST');
+    req.flush(TestAuthService.invalidAuthData);
+  });
+
+  it('should save valid state to local storage', () => {
+    service.register({}).subscribe((res) => {
+      expect(service.isLoggedIn()).toBe(true);
+      expect(localStorage.getItem(TestAuthService.storageItemId)).not.toBeNull();
+    });
+
+    const req = httpTestingController.expectOne('/api/auth/register');
+    expect(req.request.method).toEqual('POST');
+    req.flush(TestAuthService.validAuthData);
+  });
+
+  it('should logot on invalid result', () => {
+    service.register({}).subscribe(
+      () => {},
+      (err) => {
+        expect(service.isLoggedIn()).toBe(false);
+        expect(localStorage.getItem(TestAuthService.storageItemId)).toBeNull();
+        expect(err.status).toBe(404);
+      },
+    );
+
+    const req = httpTestingController.expectOne('/api/auth/register');
+    expect(req.request.method).toEqual('POST');
+    req.flush('Error message', { status: 404, statusText: 'Not Found' });
+  });
+});
+
+describe('AbstractAuthService token renewal without state', () => {
+  sharedSetup();
+
+  it('should logout', () => {
+    service.renewToken().subscribe((res) => {
+      expect(service.isLoggedIn()).toBe(false);
+      expect(localStorage.getItem(TestAuthService.storageItemId)).toBeNull();
+    });
+  });
+});
+
+describe('AbstractAuthService token renewal with invalid state', () => {
+  beforeEach(() => {
+    localStorage.setItem(TestAuthService.storageItemId, JSON.stringify(TestAuthService.invalidAuthData));
+  });
+  sharedSetup();
+
+  it('should logout', () => {
+    service.renewToken().subscribe((res) => {
+      expect(service.isLoggedIn()).toBe(false);
+      expect(localStorage.getItem(TestAuthService.storageItemId)).toBeNull();
+    });
+  });
+});
+
+describe('AbstractAuthService token renewal with valid state', () => {
+  beforeEach(() => {
+    localStorage.setItem(TestAuthService.storageItemId, JSON.stringify(TestAuthService.validAuthData));
+  });
+  sharedSetup();
+
+  it('should login on valid result', () => {
+    service.renewToken().subscribe((res) => {
+      expect(service.isLoggedIn()).toBe(true);
+      expect(localStorage.getItem(TestAuthService.storageItemId)).not.toBeNull();
+    });
+
+    const req = httpTestingController.expectOne('/api/auth/renew-token');
+    expect(req.request.method).toEqual('POST');
+    req.flush(TestAuthService.validAuthData);
+  });
+
+  it('should logout on http error', () => {
+    service.renewToken().subscribe(
+      () => {},
+      (err) => {
+        expect(service.isLoggedIn()).toBe(false);
+        expect(localStorage.getItem(TestAuthService.storageItemId)).toBeNull();
+        expect(err.status).toBe(404);
+      },
+    );
+
+    const req = httpTestingController.expectOne('/api/auth/renew-token');
+    expect(req.request.method).toEqual('POST');
+    req.flush('Error message', { status: 404, statusText: 'Not Found' });
+  });
+});
---
+++ projects/rahimalizada/ngx-jwt-auth/src/lib/auth/abstract-auth.service.ts
@@ -0,0 +1,135 @@
+import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { BehaviorSubject, Observable, Observer } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import * as shiro from 'shiro-trie';
+
+export abstract class AbstractAuthService<T extends { token: string; refreshToken: string; roles: string[] }> {
+  loggedInSubject = new BehaviorSubject<boolean>(false);
+  authDataSubject = new BehaviorSubject<T>(null);
+  shiroTrie = shiro.newTrie();
+
+  protected jwtHelper = new JwtHelperService();
+
+  constructor(private storageItemId: string, private http: HttpClient, public apiPath: string) {
+    this.authDataSubject.subscribe((authData) => {
+      if (authData) {
+        this.shiroTrie.reset();
+        this.shiroTrie.add(...authData.roles);
+      }
+    });
+
+    this.loadStorage();
+  }
+
+  private isValid(authData: T): boolean {
+    if (!authData || !authData.token) {
+      return false;
+    }
+
+    const isTokenExpired = this.isTokenExpired(authData.token);
+    const isRefreshTokenExpired = this.isTokenExpired(authData.refreshToken);
+    if (isTokenExpired && isRefreshTokenExpired) {
+      return false;
+    }
+    return true;
+  }
+
+  isTokenExpired(token: string) {
+    try {
+      const expired = this.jwtHelper.isTokenExpired(token);
+      if (expired) {
+        // console.log('Token expired: ' + this.jwtHelper.getTokenExpirationDate(token));
+        return true;
+      }
+    } catch (error) {
+      // console.log(error);
+      return true;
+    }
+    return false;
+  }
+
+  private deleteStorage() {
+    localStorage.removeItem(this.storageItemId);
+  }
+
+  private loadStorage() {
+    const authData = JSON.parse(localStorage.getItem(this.storageItemId));
+    if (authData) {
+      this.loggedInSubject.next(this.isValid(authData));
+      this.authDataSubject.next(authData);
+    }
+  }
+
+  saveStorage(authData: T) {
+    localStorage.setItem(this.storageItemId, JSON.stringify(authData));
+    this.loggedInSubject.next(this.isValid(authData));
+    this.authDataSubject.next(authData);
+  }
+
+  isLoggedIn() {
+    this.loggedInSubject.next(this.isValid(this.authDataSubject.value));
+    return this.loggedInSubject.value;
+  }
+
+  renewToken() {
+    if (!this.authDataSubject.value || !this.authDataSubject.value.refreshToken) {
+      this.logout();
+      return new Observable((observer: Observer<T>) => {
+        observer.next(null);
+        observer.complete();
+      });
+    }
+    return this.http
+      .post<T>(this.apiPath + '/renew-token', { refreshToken: this.authDataSubject.value.refreshToken })
+      .pipe(
+        tap(
+          (authData) => {
+            console.log('Token renewed, expiration date: ' + this.jwtHelper.getTokenExpirationDate(authData.token));
+            this.saveStorage(authData);
+          },
+          (error) => {
+            console.log('Token renewal failed');
+            this.logout();
+          },
+        ),
+      );
+  }
+
+  logout() {
+    this.deleteStorage();
+    this.loggedInSubject.next(false);
+    this.authDataSubject.next(null);
+  }
+
+  register(data: any) {
+    return this.http.post<T>(this.apiPath + '/register', data).pipe(
+      tap(
+        (result) => this.saveStorage(result),
+        (error) => this.logout(),
+      ),
+    );
+  }
+
+  login(data: any) {
+    return this.http.post<T>(this.apiPath + '/login', data).pipe(
+      tap(
+        (result) => this.saveStorage(result),
+        (error) => this.logout(),
+      ),
+    );
+  }
+
+  resetPasswordRequest(data: any) {
+    return this.http.post<void>(this.apiPath + '/reset-password/request', data);
+  }
+
+  resetPasswordConfirmation(data: any) {
+    return this.http.post<void>(this.apiPath + '/reset-password/confirmation', data);
+  }
+
+  // Comma separated
+  hasPermissions(requiredPermissions: string) {
+    return this.shiroTrie.check(requiredPermissions);
+  }
+}
